427e8825ac9b1715331987ff044f26f5d03020e8
Thomas <thomas.lac@outlook.com>
2017-02-05 23:11:22 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import React, { Component } from 'react'; // importing react
-
-
 import './App.css';
-
 import SearchBar from './components/search_bar';
 import UserList from './components/user_list';
 import $ from "jquery";
-var htnUsers = [];
-$.getJSON('https://hackthenorth.com/fe-users.json', function(results){
-    htnUsers = results
-});
-//console.log(htnUsers);
+
 class App extends Component {
-    //let htnUsers = $.getJSON('https://hackthenorth.com/fe-users.json');
-    //console.log(htnUsers);
+
     constructor(props){
         super(props);
-
-
         this.state = {user_results: []};
-        //this.state = {user_results: [],
-        //              htn_users: $.getJSON('https://hackthenorth.com/fe-users.json')};
         this.userSearch('');
     }
 
     userSearch(searchString){
-        //console.log({htn_users});
-        //$.getJSON('https://api.myjson.com/bins/pgxi9', (data) => {
+
         $.getJSON('https://hackthenorth.com/fe-users.json', (data) => {
-        //data is the JSON string
-        //alert(data[0].company);
+
         function getResults(userInfo) {
             let userName = userInfo.name.toLowerCase();
             searchString = searchString.toLowerCase();
-
             return userName.includes(searchString);
-
-            //return userInfo.name == searchString;
         }
 
         data = data.filter(getResults);
